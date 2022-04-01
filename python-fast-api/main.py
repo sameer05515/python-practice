336ef561cd161165c12f4200fe9b5832082ac276
sameer05515 <samee.geeee@gmail.com>
2022-04-01 09:40:23 +0530

    commit

--- python-fast-api/main.py
+++ python-fast-api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
+
 class UnicornException(Exception):
     def __init__(self, name: str):
         self.name = name
@@ -23,8 +24,61 @@ async def read_unicorn(name: str):
         raise UnicornException(name=name)
     return {"unicorn_name": name}
 
+
 # app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/interest/calc/{amount}/{interest_rate}/{year}")
+async def cal_future_amount(amount: float, interest_rate: float, year: int):
+    if amount <= 0:
+        raise UnicornException("Amount not valid : " + str(amount))
+    if interest_rate < -1 or interest_rate > 1:
+        raise UnicornException("Interest rate not valid : " + str(interest_rate))
+    if year < 0:
+        raise UnicornException("Year not valid : " + str(year))
+    fa: float = amount
+    for x in range(1, year + 1):
+        fa = fa * (1 + interest_rate)
+        print(fa)
+    return {
+        "amount": amount,
+        "interest_rate": interest_rate,
+        "years": year,
+        "future_amt": fa
+    }
+
+
+@app.get("/sip/calc/{amount}/{interest_rate}/{year}")
+async def cal_future_sip_amount(amount: float, interest_rate: float, year: int):
+    if amount <= 0:
+        raise UnicornException("Amount not valid : " + str(amount))
+    if interest_rate < -1 or interest_rate > 1:
+        raise UnicornException("Interest rate not valid : " + str(interest_rate))
+    if year < 0:
+        raise UnicornException("Year not valid : " + str(year))
+
+    return_obj = {"amount": amount, "interest_rate": interest_rate, "years": year, "year_wise_detail": []};
+    future_amount: float = 0
+    fut = []
+    for x in range(1, year + 1):
+        tot_principle = (future_amount + amount)
+        future_amount = (future_amount + amount) * (1 + interest_rate)
+        interest_credited = future_amount - tot_principle
+        year_wise_detail = {
+            "total_principle": tot_principle,
+            "interest_credited": interest_credited,
+            "year": x,
+            "total_accumulated_amount": future_amount
+        }
+        add_value(year_wise_detail, return_obj)
+        print(future_amount)
+    return_obj["future_amt"] = future_amount
+    return return_obj
+
+
+def add_value(val, dict_obj):
+    dict_obj["year_wise_detail"].append(val)
--- python-rest-api-consume/python_rest_get_api_consume1.py
+++ python-rest-api-consume/python_rest_get_api_consume1.py
@@ -3,26 +3,18 @@ import requests
 # api_url = "https://jsonplaceholder.typicode.com/todos/1"
 api_url = "https://api.github.com/users/sameer05515/repos?per_page=100&page="
 
-list =[1,2,3]
-count=1
-respList=[]
-for i in list:
-    response = requests.get(api_url+str(i))
-    json=response.json()    
-    for x in json: 
-        resp={}
-        resp['id']= x['id']
-        resp['name']= x['name']
-        resp['description']= x['description']
-        resp['language']= x['language']
-        resp['html_url']= x['html_url']
+my_list = [1, 2, 3]
+count = 1
+respList = []
+for i in my_list:
+    response = requests.get(api_url + str(i))
+    json = response.json()
+    for x in json:
+        resp = {'id': x['id'], 'name': x['name'], 'description': x['description'], 'language': x['language'],
+                'html_url': x['html_url']}
         respList.append(resp)
-        # print(str(count) ," : ", x['name'] , " : ", x['language'])
-        count=count+1
-
+        count = count + 1
 
 print(respList)
 
-
-  
 # {'userId': 1, 'id': 1, 'title': 'delectus aut autem', 'completed': False}
