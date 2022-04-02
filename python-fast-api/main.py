ddc94bf50de5cb607c0cc99dbe232c3e66925e21
sameer05515 <samee.geeee@gmail.com>
2022-04-02 15:29:55 +0530

    commit

--- python-fast-api/main.py
+++ python-fast-api/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
+from number_to_words import (convertToDigit)
+import future_sip_calculation as fsip
+
 
 class UnicornException(Exception):
     def __init__(self, name: str):
@@ -61,24 +64,4 @@ async def cal_future_sip_amount(amount: float, interest_rate: float, year: int):
     if year < 0:
         raise UnicornException("Year not valid : " + str(year))
 
-    return_obj = {"amount": amount, "interest_rate": interest_rate, "years": year, "year_wise_detail": []};
-    future_amount: float = 0
-    fut = []
-    for x in range(1, year + 1):
-        tot_principle = (future_amount + amount)
-        future_amount = (future_amount + amount) * (1 + interest_rate)
-        interest_credited = future_amount - tot_principle
-        year_wise_detail = {
-            "total_principle": tot_principle,
-            "interest_credited": interest_credited,
-            "year": x,
-            "total_accumulated_amount": future_amount
-        }
-        add_value(year_wise_detail, return_obj)
-        print(future_amount)
-    return_obj["future_amt"] = future_amount
-    return return_obj
-
-
-def add_value(val, dict_obj):
-    dict_obj["year_wise_detail"].append(val)
+    return fsip.cal_future_sip_amount(amount, interest_rate, year)
